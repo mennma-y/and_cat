a1436de8a5caa46737b99c6e63648ddd00f2aa34
a925s <a_925s@yahoo.co.jp>
2022-02-27 12:44:06 +0900

    20220227西山　CSS、Controller完成

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('main.home');
+});
+
+Route::get('/cat/profile', function () {
+    return view('main.cat_profile');
+});
+
+Route::get('/like', function () {
+    return view('main.like');
+});
+
+Route::get('/search', function () {
+    return view('main.search');
+});
+
+
+
+
+Route::get('/admin/cat/register', function () {
+    return view('admin.cat_register');
+});
+
+Route::post('/admin/cat/register', 'CatController@catRegister');
