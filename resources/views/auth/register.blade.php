15c0d711ee4d72cded07b17b31f8bcb8e71e90ab
mennma-y <y.exlovekb.06.0211@gmail.com>
2022-04-01 15:48:51 +0900

    20220401 宮本 ヘッダーCSS修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -41,7 +41,7 @@
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
-                                @enderror
+                                @enderror  
                             </div>
                         </div>
 
